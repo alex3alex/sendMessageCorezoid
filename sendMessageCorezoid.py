28cc54b2591c2fa6bc83b9f6173c95d24aba133b
Arfey <misha.gavela@dataroot.co>
2016-05-09 06:13:01 +0300

    Update sendMessageCorezoid.py

--- sendMessageCorezoid.py
+++ sendMessageCorezoid.py
@@ -1,34 +1,17 @@
-import time
-import os
-import json
+import subprocess
 
 
-def sendMessageCorezoid(message):
+def sendMessageCorezoid(template, email):
     """
     Send message, using Corezoid backend.
     Arguments is object with keys: day, email, idTemplate, key, user.
     """
-    conv_id = 136950
-    conv_user_id = "76793"
-    conv_user_pass = '3uaGMeL2WqXU5ptPOarhAgLH6Ff5Czfdds8oJtFlu0sJPlq9Lg'
-    time_secund = int(time.time())
+    data = '{ "ops" : [{ "type" : "create", "obj" : "task", "conv_id" : \
+        136950, "data" : {"day":"1","email":"%s","idTemplate":"%s","key":\
+        "AxYXSJ92xN4ZEdLtLh0_4g","user":"Igor"} }] }' % (email, template)
     url = 'https://www.corezoid.com/api/1/json/'
-    data = {}
-    data["type"] = "create"
-    data["obj"] = "task"
-    data["conv_id"] = 136950
-    data["data"] = message
-    json_data = json.dumps({"ops": [data]})
-    openssl = os.popen("echo '%s%s%s%s' \
-                        | openssl dgst -sha1 | awk '{print $NF}'"
-                       % (time_secund, conv_user_pass, json_data,
-                          conv_user_pass)).read()[:-1]
-    os.popen("curl --silent -XPOST %s%s/%s/%s --data %s"
-             % (url, conv_user_id, time_secund, openssl, json_data))
-
+    subprocess.Popen('sh shell.sh %s %s' % (template, email), shell=True).wait()
+    print('done')
 
 # example
-
-# sendMessageCorezoid(
-#     {"day": "1", "email": "igor.sizon@dataroot.co", "idTemplate":
-#      "telegram", "key": "AxYXSJ92xN4ZEdLtLh0_4g", "user": "Igor"})
+sendMessageCorezoid('telegram', 'misha.gavela@dataroot.co')
